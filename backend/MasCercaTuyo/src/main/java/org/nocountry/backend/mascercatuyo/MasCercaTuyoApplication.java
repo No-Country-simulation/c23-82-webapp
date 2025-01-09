e65984bbb1f058f5ce19e6ccfb3156c096afa2c5
Santiago <santimh04@gmail.com>
2025-01-09 14:25:25 -0500

    Creación del proyecto Spring Boot para el backend de Más Cerca Tuyo

---
+++ backend/MasCercaTuyo/src/main/java/org/nocountry/backend/mascercatuyo/MasCercaTuyoApplication.java
@@ -0,0 +1,13 @@
+package org.nocountry.backend.mascercatuyo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasCercaTuyoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MasCercaTuyoApplication.class, args);
+    }
+
+}
---
+++ backend/MasCercaTuyo/src/test/java/org/nocountry/backend/mascercatuyo/MasCercaTuyoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.nocountry.backend.mascercatuyo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MasCercaTuyoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
