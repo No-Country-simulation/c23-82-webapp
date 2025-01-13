6bc8d18820f64dc63118e22e5b566b61edfb2302
renn71 <braianmartinez648@gmail.com>
2025-01-13 18:34:52 -0400

    fix:move files

--- frontend/MasCercaTuyo/eslint.config.js
+++ frontend/MasCercaTuyo/eslint.config.js
@@ -1,43 +1,61 @@
-import js from '@eslint/js'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import globals from 'globals'
-import tseslint from 'typescript-eslint'
+import js from '@eslint/js';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 
-export default tseslint.config(
-  { ignores: ['dist'] },
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+export default [
   {
-    extends: [
-      js.configs.recommended,
-      ...tseslint.configs.recommended,
-      'eslint:recommended',
-      'plugin:@typescript-eslint/recommended-type-checked',
-      'plugin:@typescript-eslint/stylistic-type-checked',
-      'plugin:react/recommended',
-      'plugin:react/jsx-runtime',
-      'plugin:react-hooks/recommended',
-    ],
+    ignores: ['dist', 'node_modules'],
+  },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
-      globals: globals.browser,
+      sourceType: 'module',
+      parser: tseslint.parser,
+      parserOptions: {
+        projectService: {
+          rootDir: __dirname,
+          tsConfigPath: './tsconfig.json',
+        },
+      },
+      globals: {
+        ...globals.browser,
+      },
     },
     plugins: {
+      '@typescript-eslint': tseslint.plugin,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
-    },
-    parserOptions: {
-      ecmaVersion: 'latest',
-      sourceType: 'module',
-      proyecto: ['./tsconfig.json', './tsconfig.node.json'],
-      tsconfigRootDir: __dirname,
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
     },
   },
-)
+  {
+    files: ['vite.config.ts'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      sourceType: 'module',
+      parser: tseslint.parser,
+      parserOptions: {
+        projectService: {
+          rootDir: __dirname,
+          tsConfigPath: './tsconfig.node.json',
+        },
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tseslint.plugin,
+    },
+  },
+];
