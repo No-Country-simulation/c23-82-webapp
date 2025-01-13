c9780b1416af0b8db584c22cbec70a2a15f0193d
CARLOSMARES <46010587+CARLOSMARES@users.noreply.github.com>
2025-01-13 15:26:44 -0600

    instalacion de daisy.ui, tailwind, husky, eslint, axios, js-cookie

--- frontend/MasCercaTuyo/eslint.config.js
+++ frontend/MasCercaTuyo/eslint.config.js
@@ -1,13 +1,22 @@
 import js from '@eslint/js'
-import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import globals from 'globals'
 import tseslint from 'typescript-eslint'
 
 export default tseslint.config(
   { ignores: ['dist'] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommended,
+      'eslint:recommended',
+      'plugin:@typescript-eslint/recommended-type-checked',
+      'plugin:@typescript-eslint/stylistic-type-checked',
+      'plugin:react/recommended',
+      'plugin:react/jsx-runtime',
+      'plugin:react-hooks/recommended',
+    ],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
@@ -23,6 +32,12 @@ export default tseslint.config(
         'warn',
         { allowConstantExport: true },
       ],
+    },
+    parserOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      proyecto: ['./tsconfig.json', './tsconfig.node.json'],
+      tsconfigRootDir: __dirname,
     },
   },
 )
