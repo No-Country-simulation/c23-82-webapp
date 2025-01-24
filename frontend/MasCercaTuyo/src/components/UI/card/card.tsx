8869eb2d954b544defad3c9f1ace14b2e35da983
CARLOSMARES <46010587+CARLOSMARES@users.noreply.github.com>
2025-01-24 12:23:08 -0600

    feat: remove unused CSS styles and implement card component for better UI structure

---
+++ frontend/MasCercaTuyo/src/components/UI/card/card.tsx
@@ -0,0 +1,19 @@
+interface Props {
+    children: React.ReactNode;
+    height?: string;
+    width?: string;
+    background?: string;
+    foreground?: string;
+    margin?: string;
+    padding?: string;
+}
+
+function card(props: Props) {
+    return (
+        <div className={`card border ${props.width} ${props.height} ${props.background} ${props.foreground} ${props.margin} ${props.padding}`}>
+            {props.children}
+        </div>
+    )
+}
+
+export default card;
--- frontend/MasCercaTuyo/src/components/UI/menu/menu.tsx
+++ frontend/MasCercaTuyo/src/components/UI/menu/menu.tsx
@@ -1,7 +1,9 @@
 function menu() {
     return (
-        <>
-        </>
+        <div style={{ display: "flex", alignItems: "center", gap: "1rem" }}>
+            <img src="svg/logo.svg" alt="logo" width={40} />
+            <input type="search" placeholder="Search" style={{ backgroundColor: 'white', border: 'solid 1px black', borderRadius: '1rem', padding: '0.5rem' }} />
+        </div>
     )
 }
 
