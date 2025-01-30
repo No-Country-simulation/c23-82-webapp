acc314828061aabdf30557e865807ee9a7df57a1
Barbara <0deabreubarbara@gmail.com>
2025-01-30 08:17:09 -0500

    feat: register and components for it

---
+++ frontend/MasCercaTuyo/src/components/ui/UploadPhoto.tsx
@@ -0,0 +1,51 @@
+import { useCallback, useState } from 'react';
+import { useDropzone } from 'react-dropzone';
+import { uploadPhoto } from '../../services/api.service';
+
+const UploadPhoto = ({ onImageUpload }) => {
+  const [image, setImage] = useState(null);
+  console.log('image', image);
+
+  const onDrop = useCallback(
+    async acceptedFiles => {
+      const file = acceptedFiles[0];
+      setImage(URL.createObjectURL(file));
+
+      try {
+        const imageUrl = await uploadPhoto(file);
+        console.log("🚀 ~ UploadPhoto ~ imageUrl:", imageUrl)
+        onImageUpload(imageUrl);
+      } catch (error) {
+        console.error(error);
+      }
+    },
+    [onImageUpload]
+  );
+
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({
+    onDrop,
+    accept: ['image/*'],
+    multiple: false,
+  });
+
+  return (
+    <div
+      {...getRootProps()}
+      className={`flex flex-col items-center justify-center border-2 border-dashed 
+                        border-gray-400 rounded-lg w-40 h-40 p-4 mx-auto mb-4
+                        ${isDragActive ? 'bg-gray-200' : 'bg-gray-100'}`}
+    >
+      <input {...getInputProps()} />
+      {image ? (
+        <img src={image} alt="Foto subida" className="object-cover w-full h-full rounded-lg" />
+      ) : (
+        <div className="text-center text-gray-500 flex flex-col items-center">
+          <span className="text-4xl mb-2">📸</span>
+          <p className="text-sm">Agregar una foto</p>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default UploadPhoto;
