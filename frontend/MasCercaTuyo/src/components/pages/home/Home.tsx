a9c477719c8c54fbfa36d87d9f5a6039d7ad7fc7
CARLOSMARES <46010587+CARLOSMARES@users.noreply.github.com>
2025-01-30 09:30:21 -0600

    feat: add Menu and Footer components to Home page

--- frontend/MasCercaTuyo/src/components/pages/home/Home.tsx
+++ frontend/MasCercaTuyo/src/components/pages/home/Home.tsx
@@ -1,9 +1,13 @@
+import Footer from '../../UI/footer/footer.tsx';
+import Menu from '../../UI/menu/menu.tsx';
 import { NavBar } from '../../UI/navbar/NavBar.tsx';
 
 export function Home() {
   return (
     <>
+      <Menu />
       <NavBar />
+      <Footer />
     </>
   );
 }
